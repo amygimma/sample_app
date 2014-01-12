1a3daf7ecb0f987d9bca6f3740911082ca0fbb8b
amyg225 <amyg225@gmail.com>
2014-01-11 23:30:38 -0500

    Add user microposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -70,7 +70,7 @@ describe "Authentication" do
           end
         end
       end
-
+      
       describe "in the Users controller" do
 
         describe "visiting the edit page" do
@@ -87,8 +87,8 @@ describe "Authentication" do
           before { visit users_path }
           it { should have_title('Sign in') }
          end
-      end
-    end
+       end
+     end
     
     describe "as wrong user" do
       let(:user) { FactoryGirl.create(:user) }
@@ -107,8 +107,21 @@ describe "Authentication" do
         specify { expect(response).to redirect_to(root_url) }
       end
     end
+    
+    describe "in the Microposts controller" do
+
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+      end
  
-  .
+  
     describe "as non-admin user" do
       let(:user) { FactoryGirl.create(:user) }
       let(:non_admin) { FactoryGirl.create(:user) }
@@ -122,5 +135,5 @@ describe "Authentication" do
     end
   end
 end
-end
+
   
